95f1e13e586bbcd7dbc1222a31bb77d844f4bb07
AidanThomas <aidanjthomas97@gmail.com>
2022-10-03 15:14:28 +0100

    Refactored Hello function

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
 	http.HandleFunc("/helloworld", helloWorld)
 	http.HandleFunc("/", hello)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(name string) string {
+	if name == "" {
+		return ""
+	}
+
+	return fmt.Sprintf(message(), name)
+}
+
+func message() string {
+	formats := []string{"Hello %s!", "Welcome %s!", "Hi %s!"}
+
+	index := rand.Intn(len(formats))
+
+	return formats[index]
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -19,10 +41,9 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func hello(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Path[1:]
 
-	if name != "" {
-		fmt.Fprintf(w, "Hello %s! Nice to meet you!", name)
-		return
+	if name == "" {
+		name = "World"
 	}
 
-	fmt.Fprintln(w, "Hello! Nice to meet you!")
+	fmt.Fprintln(w, Hello(name))
 }

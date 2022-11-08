2a7d014974ca10d483a0df9fd293a46421d57e21
cmilhench <cmilhench@gmail.com>
2022-11-08 11:58:58 +0000

    Added auth and logging middleware

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/AyJayTee/greetings/internal/server"
 )
 
 func main() {
-
+	log.Println("Running...")
 	port := flag.String("p", ":8080", "port")
 	flag.Parse()
 
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,10 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/AyJayTee/greetings"
 )
@@ -16,68 +19,99 @@ type ReturnURL struct {
 	Url string `json:"url"`
 }
 
+type Database struct {
+	data []string
+}
+
 func ServiceStart(port string) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/helloworld", helloWorld)
 
+	s := &Database{
+		data: make([]string, 0),
+	}
+
 	// POST /store {"url": "http://medium.com"}
 	// {"url": "http://localhost:8080/id/abc123"}
-	mux.HandleFunc("/store", store)
+	mux.Handle("/store", storeHandler(s))
 
 	// GET /id/abc123
 	// Status 302 location: http://medium.com
-	mux.HandleFunc("/id/", id)
+	mux.Handle("/id/", idHandler(s))
 
 	mux.HandleFunc("/", hello)
 
-	http.ListenAndServe(port, mux)
+	http.ListenAndServe(port, auth("user", "pass", logging(mux)))
 }
 
-func store(w http.ResponseWriter, r *http.Request) {
+func storeHandler(store *Database) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-	// Guard clause
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
+		// Guard clause
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
 
-	var storeUrl StoreURL
+		var storeUrl StoreURL
 
-	// Deserialize JSON from body
-	if err := json.NewDecoder(r.Body).Decode(&storeUrl); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+		// Deserialize JSON from body
+		if err := json.NewDecoder(r.Body).Decode(&storeUrl); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// Validation
+		if storeUrl.Url == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if !strings.HasPrefix(storeUrl.Url, "https://") {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		store.data = append(store.data, storeUrl.Url)
 
-	// Create unique ID
-	returnUrl := ReturnURL{Url: "http://localhost:8080/id/abc123"}
+		// Create unique ID
+		returnUrl := ReturnURL{Url: fmt.Sprintf("http://localhost:8080/id/%d", len(store.data))}
 
-	// Return JSON payload
-	if err := json.NewEncoder(w).Encode(returnUrl); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+		// Return JSON payload
+		if err := json.NewEncoder(w).Encode(returnUrl); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
 }
 
-func id(w http.ResponseWriter, r *http.Request) {
-	// Guard clause
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
+func idHandler(store *Database) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			// Guard clause
+			if r.Method != http.MethodGet {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
 
-	// Read abc123 from path
-	path := r.URL.Path[4:]
+			// Read abc123 from path
+			path := r.URL.Path[4:]
+			// Guard
+			i, err := strconv.Atoi(path)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if i > len(store.data) {
+				log.Printf("OUt of bounds, %d, %d", i, len(store.data))
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			// Guard
+			url := store.data[i-1]
 
-	// Guard
-	if path != "abc123" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Set status and location header
-	w.Header().Set("Location", "http://medium.com")
-	w.WriteHeader(http.StatusFound)
+			// Set status and location header
+			w.Header().Set("Location", url)
+			w.WriteHeader(http.StatusFound)
+		})
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -92,4 +126,30 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintln(w, greetings.Hello(name))
+}
+
+func auth(user, password string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		usr, pwd, ok := r.BasicAuth()
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if usr != user {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		if pwd != password {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s", r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
